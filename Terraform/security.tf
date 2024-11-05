b6fe857a3381e8e21ec37fae4439aee2194ed05b
Szekely <szekelyboti1@gmail.com>
2024-11-05 16:55:09 +0100

    Completed Terraform configuration files

---
+++ Terraform/security.tf
@@ -0,0 +1,29 @@
+data "http" "my_ip" {
+  url = "https://api.ipify.org"
+}
+
+resource "aws_security_group" "app_security_group" {
+  name        = "app_security_group"
+  description = "Allow SSH and other necessary ports"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${data.http.my_ip.body}/32"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
